0e6234c1a21566a82c3c74a688a3a2be4e1c002f
benja78945 <104796060+benja78945@users.noreply.github.com>
2025-09-25 15:05:47 -0300

    practica y teoria de expresiones y compuestos

---
+++ condiconales-compuestas/compuestos.js
@@ -0,0 +1,11 @@
+let num = 15;
+if (num >= 10 && num<= 20) {
+    console.log ("el numero esta entre 10 y 20");
+}
+// lo que estamos diciendo con este condigo es: Si el numero es mayor o igual a 10 y menor o igual a 20 entonces imprima "el numero esta entre 10 y 20"
+// para que una expresion con && sea verdadera, ambos lados deben ser verdaderos y por encambio con || solo se necesita una verdadera 
+
+// true && true = true                          true  || true = true
+// true && false = false                        true  || false = true 
+// false && true = false                        false || true = true
+// false && false = true                        false || false = false
