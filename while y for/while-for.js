696714c7fad4bc437b5bd76af96f5b2139a5f0d6
benja78945 <104796060+benja78945@users.noreply.github.com>
2025-09-25 21:45:14 -0300

    practicas while y for

---
+++ while y for/while-for.js
@@ -0,0 +1,14 @@
+let i = 100;
+do  {
+    console.log (i)
+    i = i-5;
+}
+while (i >=0) {
+    console.log ("fin del ciclo")
+}
+// resta de 100 a 0 en diferentes bucles
+
+for (let i = 100;i>=0;i--) {
+    console.log(i)
+    console.log("-1")
+}
